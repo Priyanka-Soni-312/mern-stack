c11eb7d7f0713d0a14fd325bb82542e3addbd0c9
priyanka-soni377 <priyankasoni0377@gmail.com>
2024-07-12 16:25:51 +0530

    marn

---
+++ api/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    password:{
+        type: String,
+        required: true,
+        
+    },
+    avatar: {
+        type: String,
+        default: "https://www.shutterstock.com/image-vector/man-icon-vector-260nw-1040084344.jpg",
+    },
+
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
